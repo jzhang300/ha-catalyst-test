233239a67afb8aac53a2c713dec63a653409e146
James Zhang <james.zhang75024@gmail.com>
2017-08-08 17:50:41 -0500

    add time

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const http = require('http');
+const sqrtSeconds = require('./squareRootSeconds');
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end(`${sqrtSeconds()}`);
 });
 
 server.listen(3000, 'localhost');
